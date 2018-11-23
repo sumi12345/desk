9db3501ca3f8d5dc912797e2c3809021d180594b
sumi12345 <753007687@qq.com>
2018-11-23 22:32:02 +0800

    08 components within component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+Vue.component('task-list', {
+    template: `<ul>
+        <task v-for="task in tasks">{{ task.name }}</task>
+    </ul>`,
+    data() {
+        return {
+            tasks: [
+                { name: 'Go to the store', complete: false },
+                { name: 'Go to the email', complete: true },
+                { name: 'Go to the farm', complete: true },
+                { name: 'Go to work', complete: false },
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
