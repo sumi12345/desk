4f52f79db4f5ea10c7d0a7289acda01f735cb8d9
sumi12345 <753007687@qq.com>
2018-11-29 16:14:33 +0800

    09 practical component exercise 1: message

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+// 09 practical component exercise 1: message
+Vue.component('message', {
+    template: `
+    <div class="alert alert-warning" v-show="isVisible">
+        <button @click="hideModal" class="close" data-dismiss="alert"><span>&times;</span></button>
+        <strong>{{ title }}</strong> {{ body }}
+    </div>`,
+
+    props: ['title', 'body'],
+
+    data() {
+        return {
+            isVisible: true
+        };
+    },
+
+    methods: {
+        hideModal() {
+            this.isVisible = false;
+        }
+    }
+});
+
+// 08 component within component
 Vue.component('task-list', {
     template: `<ul>
         <task v-for="task in tasks">{{ task.name }}</task>
@@ -14,6 +38,7 @@ Vue.component('task-list', {
     }
 });
 
+// 07 component
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
